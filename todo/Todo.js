0dd8f29ba91034e495cf3d63ad97ff716ad5504e
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-09-26 13:59:18 +0200

    moved internal model logic outside of dijit class

--- todo/Todo.js
+++ todo/Todo.js
@@ -17,51 +17,19 @@ define([
 	return declare([Widget, Evented, Templated, WidgetsInTemplate, ModelRenderer], {
 		templateString: template,
 		
-		postCreate: function(){
-			this.inherited(arguments);
-			//update label
-			this.labelWidget.on("change", this.changeLabel.bind(this));
-			this.checkWidget.on("change", this.checkChanged.bind(this));		
+		_setDataAttr: function(data){ //external data
+			var todo = new TodoModel(data);
+			this.set("model", todo);
 		},
-		
-		modelMapping: {
-			label: 'label',
-			checked: 'checked'
-		},
-		
-		_setModelAttr: function(item) {
-			var todo = new TodoModel();
-			statefulSync(item, todo, {
-				label: 'label',
-				checked: 'checked'
+		_setModelAttr: function(model){ //internal model
+			this.model = model;
+			statefulSync(model, this.labelWidget, {
+				label: "value",
 			});
-			this._set("model", todo);
-			this._bindModel();
-			return this;
+			statefulSync(model, this.checkWidget, {
+				checked: "checked",
+			});
 		},
-		
-		_setLabelAttr: function(label) {
-			this.labelWidget.set("value", label, false);
-		},
-		
-		changeLabel: function() {
-			try {
-				this.get('model').set('label', this.labelWidget.get('value'));
-			}
-			catch(ex) {
-				this.set('label', this.get('model').get('label'));
-			}
-		},
-		
-		_setCheckedAttr: function(checked) {
-			domClass.toggle(this.domNode, 'checked', checked);
-			this.checkWidget.set('checked', checked, false);
-		},
-		
-		checkChanged: function(checked) {
-			this.set('checked', checked);
-			this.get('model').set('checked', this.checkWidget.get('checked'));
-		},
-		
+				
 	});
 });
--- todo/model/Todo.js
+++ todo/model/Todo.js
@@ -1,12 +1,26 @@
 define([
-	"SkFramework/model/Model",
-], function(Model){
+	"dojo/_base/declare",
+	"dojo/Stateful",
+], function(declare, Stateful){
 	
-	return Model.extend("Todo", {
+	return declare(Stateful, {
 		_checkedGetter: function() {
 			return this.checked || false;
 		},
-	});
+
+		_labelSetter: function(value) {
+			if(this.get("checked")===true){
+				throw ("Cannot change the label of an already completed todo");
+			} else {
+				this.label = value;
+			}
+		},
+		//is that a good idea to implement that in model ?
+/*		_setCheckedAttr: function(checked) {
+			domClass.toggle(this.domNode, 'checked', checked);
+			this.checkWidget.set('checked', checked, false);
+		},
+*/	});
 
 
 });
