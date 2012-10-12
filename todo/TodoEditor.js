dc43429f2643aef12c63268b0896a29fbd42a4a4
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-10-12 07:18:27 +0200

    use a component class that encapsulate View and Presenter instead of mixin them together

--- todo/TodoEditor.js
+++ todo/TodoEditor.js
@@ -1,64 +1,112 @@
 define([
-	"dojo/_base/declare", "dojo/_base/lang",
-	"dijit/_WidgetBase",	"dijit/_TemplatedMixin",	"dijit/_WidgetsInTemplateMixin",
+	"dojo/_base/declare",	"dojo/_base/lang",	'dojo/Stateful',
+	'dijit/Destroyable',	"dijit/_WidgetBase",	"dijit/_TemplatedMixin",	"dijit/_WidgetsInTemplateMixin",
 	"dojo/text!./template.html",
-	"SkFramework/utils/binding",
-	"./Presenter",
+	"SkFramework/utils/binding",	"SkFramework/utils/statefulSync",
+	"skTodos/model/domain/Todo",
 
 	
 	"dijit/form/TextBox",	"dijit/form/CheckBox",
 	"skTodos/dueDatePicker/DueDatePicker",
 ], function(
-	declare, lang,
-	Widget,					Templated,					WidgetsInTemplate,
+	declare,				lang,				Stateful,
+	Destroyable,			Widget,					Templated,					WidgetsInTemplate,
 	template,
-	binding,
-	Presenter
+	binding,						statefulSync,
+	Todo
 ){
+	var Presenter = declare([Stateful, Destroyable], {
+		constructor: function(params){
+			this.todo = null;
+			this.disabled = null;
+			this.todoStatefulSyncHandler = {
+				remove: function(){},
+			};
+		},
+		_disabledGetter: function() {
+			return this.disabled || false;
+		},
 
-	return declare([Widget, Templated, WidgetsInTemplate, Presenter], {
+		_todoSetter: function(value){
+			var todo;
+			this.todoStatefulSyncHandler.remove();
+			if(value instanceof Todo){
+				todo = value;
+			} else {
+				todo = new Todo(value);
+			}
+			//store the value
+			this.todo = todo;
+			//explose it
+			this.todoStatefulSyncHandler = statefulSync(this.todo, this, {
+				label: "label",
+				checked: "checked",
+				dueDate: "dueDate",
+			});
+			this.own(this.todoStatefulSyncHandler);
+		},
+		setDueDateToToday: function(){
+			this.set("dueDate", new Date());
+		},
+	});
+
+	var View = declare([Widget, Templated, WidgetsInTemplate], {
 		templateString: template,
-
-		constructor: function(){
-			this.disabled = false; 
-		},
-
-		startup: function(){
-			this.inherited(arguments);
-			this.bind();
-		},
-
-		bind: function(){ this.own(
-			new binding.Multi(this, this.labelWidget, [
-				{type: "Value", sourceProp: "checked", targetProp: "disabled"},
-				{type: "ValueSync", sourceProp: "label", targetProp: "value"},
-				{type: "Value", sourceProp: "disabled", targetProp: "disabled"},
-
-			]),
-			new binding.Multi(this, this.checkWidget, [
-				{type: "ValueSync", sourceProp: "checked", targetProp: "checked"},
-				{type: "Value", sourceProp: "disabled", targetProp: "disabled"},
-			]),
-			new binding.Multi(this, this.dueDateWidget, [
-				{type: "ValueSync", sourceProp: "dueDate", targetProp: "date"},
-				{type: "Value", sourceProp: "disabled", targetProp: "disabled"},
-			]),
-			new binding.Click(this.dueDateButton, this, {
-				method: "setDueDateToToday"
-			}),
-			new binding.Display(this, this.dueDateWidget, {
-				sourceProp: "dueDate"
-			}),
-			new binding.Display(this, this.dueDateButton, {
-				sourceProp: "dueDate", not: true
-			})
-		);},
 
 		set: function() {
 			if (!this._destroyed) {
 				this.inherited(arguments);
 			}
 		}
-						
+	});
+
+	return declare([Stateful, Destroyable], {
+		templateString: template,
+
+		constructor: function(params){
+			this.disabled = false;
+			this.presenter = new Presenter();
+			this.view = new View();
+			this.view.startup();
+			this.bind();
+		},
+		destroy: function(){
+			this.view.destroy();
+		},
+
+		get: function() {
+			return this.presenter.get.apply(this.presenter, arguments);
+		},
+
+		set: function() {
+			return this.presenter.set.apply(this.presenter, arguments);
+		},
+
+		bind: function() {
+			this.own(
+				new binding.Multi(this.presenter, this.view.labelWidget, [
+					{type: "Value", sourceProp: "checked", targetProp: "disabled"},
+					{type: "ValueSync", sourceProp: "label", targetProp: "value"},
+					{type: "Value", sourceProp: "disabled", targetProp: "disabled"}
+				]),
+				new binding.Multi(this.presenter, this.view.checkWidget, [
+					{type: "ValueSync", sourceProp: "checked", targetProp: "checked"},
+					{type: "Value", sourceProp: "disabled", targetProp: "disabled"}
+				]),
+				new binding.Multi(this.presenter, this.view.dueDateWidget, [
+					{type: "ValueSync", sourceProp: "dueDate", targetProp: "date"},
+					{type: "Value", sourceProp: "disabled", targetProp: "disabled"}
+				]),
+				new binding.Click(this.view.dueDateButton, this.presenter, {
+					method: "setDueDateToToday"
+				}),
+				new binding.Display(this.presenter, this.view.dueDateWidget, {
+					sourceProp: "dueDate"
+				}),
+				new binding.Display(this.presenter, this.view.dueDateButton, {
+					sourceProp: "dueDate", not: true
+				})
+			);
+		}
 	});
 });
