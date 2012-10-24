5fc6d3b4bba9016122d7016cd1208e353e5fd86b
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-10-24 08:55:20 +0200

    converted TodoEditor and associated test to the new implementation of SkComponent

--- todo/TodoEditor.js
+++ todo/TodoEditor.js
@@ -2,20 +2,23 @@ define([
 	"dojo/_base/declare",	"dojo/_base/lang",	'dojo/Stateful',
 	'dijit/Destroyable',	"dijit/_WidgetBase",	"dijit/_TemplatedMixin",	"dijit/_WidgetsInTemplateMixin",
 	"dojo/text!./template.html",
-	"SkFramework/component/Component",	'SkFramework/component/Presenter',
+	"SkFramework/component/Component",	'SkFramework/component/_Dom', 'SkFramework/component/Presenter',
 	"SkFramework/utils/binding",	"SkFramework/utils/statefulSync",
 	"skTodos/model/domain/Todo",
 
 	
-	"dijit/form/TextBox",	"dijit/form/CheckBox",
+	"dijit/form/TextBox",	"dijit/form/CheckBox", "dijit/form/Button",
 	"skTodos/dueDatePicker/DueDatePicker",
 ], function(
 	declare,				lang,				Stateful,
 	Destroyable,			Widget,					Templated,					WidgetsInTemplate,
 	template,
-	Component,							Presenter,
+	Component,	_Dom,						Presenter,
 	binding,						statefulSync,
-	Todo
+	Todo,
+	TextBox, CheckBox, Button,
+	DueDatePicker
+
 ){
 	var TodoPresenter = declare([Presenter], {
 		constructor: function(params){
@@ -77,39 +80,81 @@ define([
 		}
 	});
 
-	return declare([Component], {
+	return declare([Component, _Dom], {
+		tag: "div",
+		domNodeAttrs: {
+			class: "todo"
+		},
+
 		constructor: function(params){
 			this.disabled = false;
 			this._presenter = new TodoPresenter();
-			this.view = new View();
-			this.view.startup();
+			//register components (but don't place them)
+			this._addComponents({
+				checkWidget: new CheckBox({
+					//hack to prevent widget to call set when it is destroyed
+					set: function(){
+						if (!this._destroyed) {
+							CheckBox.prototype.set.apply(this, arguments);
+						}
+					}
+				}),
+				labelWidget: new TextBox({
+					//hack to prevent widget to call set when it is destroyed
+					set: function(){
+						if (!this._destroyed) {
+							TextBox.prototype.set.apply(this, arguments);
+						}
+					}
+
+				}),
+				dueDateButton: new Button({label: "add a due date"}),
+				dueDateWidget: new DueDatePicker(),
+			});
+		},
+
+		render: function(){
+			this.inherited(arguments);
+			//place components
+			this._append(this._components.checkWidget);
+			this._append(this._components.labelWidget);
+			this._append(this._components.dueDateButton);
+			this._append(this._components.dueDateWidget);
+
 		},
 		
 		bind: function() {
 			this.own(
-				new binding.Multi(this._presenter, this.view.labelWidget, [
+				new binding.Multi(this._presenter, this._components.labelWidget, [
 					{type: "Value", sourceProp: "checked", targetProp: "disabled"},
 					{type: "ValueSync", sourceProp: "label", targetProp: "value"},
 					{type: "Value", sourceProp: "disabled", targetProp: "disabled"}
 				]),
-				new binding.Multi(this._presenter, this.view.checkWidget, [
+				new binding.Multi(this._presenter, this._components.checkWidget, [
 					{type: "ValueSync", sourceProp: "checked", targetProp: "checked"},
 					{type: "Value", sourceProp: "disabled", targetProp: "disabled"}
 				]),
-				new binding.Multi(this._presenter, this.view.dueDateWidget, [
+				new binding.Multi(this._presenter, this._components.dueDateWidget, [
 					{type: "ValueSync", sourceProp: "dueDate", targetProp: "date"},
 					{type: "Value", sourceProp: "disabled", targetProp: "disabled"}
 				]),
-				new binding.Click(this.view.dueDateButton, this._presenter, {
+				new binding.Click(this._components.dueDateButton, this._presenter, {
 					method: "setDueDateToToday"
 				}),
-				new binding.Display(this._presenter, this.view.dueDateWidget, {
+				new binding.Display(this._presenter, this._components.dueDateWidget, {
 					sourceProp: "dueDate"
 				}),
-				new binding.Display(this._presenter, this.view.dueDateButton, {
+				new binding.Display(this._presenter, this._components.dueDateButton, {
 					sourceProp: "dueDate", not: true
 				})
 			);
+		},
+
+		set: function() {
+			if (!this._destroyed) {
+				this.inherited(arguments);
+			}
 		}
+
 	});
 });
