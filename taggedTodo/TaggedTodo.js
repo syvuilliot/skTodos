5123c28714775c0fc817f369dee40c79a4e07713
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-10-30 14:36:37 +0100

    add tag picker in taggedTodo widget

--- taggedTodo/TaggedTodo.js
+++ taggedTodo/TaggedTodo.js
@@ -1,29 +1,63 @@
 define([
 		'dojo/_base/declare',
 		'dojo/dom-construct',
-		'SkFramework/component/DomComponent',	'SkFramework/component/_WithDomNode',
+		'SkFramework/component/DomComponent',	'SkFramework/component/_WithDomNode', "SkFramework/component/_WithDijit",
 		'SkFramework/component/Presenter',
 		'SkFramework/utils/binding',
 		"../todo/TodoEditor",
 		"../removableList/List",
+		"dijit/form/ComboBox",
+		"dojo/store/Memory",
+		"dojo/store/Observable",
 ], function(
 		declare,
 		domConstruct,
-		DomComponent, _WithDom,
+		DomComponent, _WithDom, _WithDijit,
 		PresenterBase,
 		binding,
 		TodoEditor,
-		RemovableList
+		RemovableList,
+		ComboBox,
+		Memory,
+		Observable
 	){
 
 	var Presenter = declare([PresenterBase], {
+		tagLabel: "",
+		//this solution for maintening a list of tags not already linked to a todo is not robust
+/*		constructor: function(){
+			this.remainingTagsStore = Observable(new Memory());
+			this.own(
+				new binding.ObservableQueryResult(this, this, {
+					sourceProp: "tags",
+					addMethod: "addRemainingTag",
+					removeMethod: "removeRemainingTag",
+				}),
+				new binding.ObservableQueryResult(this, this, {
+					sourceProp: "todoTags",
+					addMethod: "removeRemainingTag",
+					removeMethod: "addRemainingTag",
+				})
+			);
+		},
+
+		addRemainingTag: function(tag){
+			this.remainingTagsStore.put(tag);
+		},
+		removeRemainingTag: function(tag){
+			this.remainingTagsStore.remove(tag.id);
+		},
+*/
 		_valueSetter: function(value){
 			//TODO: convert value to an instance of Todo if necesary
 			var todo = this.value = value;
 			this.set("todo", todo);
+			this.set("todoTags", todo.get("tags"));
+
+			//**** temporary
 			function tags2string(tags){
 				return tags.length ? tags.reduce(function(tagsString, tag){
-					return tagsString+tag.get("label");
+					return tagsString+tag.get("label")+", ";
 				}, ""): "";
 			}
 			var tagsList = todo.get("tags");
@@ -32,12 +66,32 @@ define([
 			tagsList.observe(function(){
 				this.set("tagsString", tags2string(todo.get("tags")));
 			}.bind(this));
-
+			//***** temporary
+		},
+		_tagsSetter: function(value){
+			this.tags = value;
+			this.set("remainingTagsStore", value);
+		},
+		tagLabelSubmited: function(){
+			var label = this.get("tagLabel");
+			if (label){
+				//get existing tag by label
+				var tag = Tag.query({label: label})[0];
+				//or create a new one
+				if (!tag) {
+					tag = new Tag({label: label}).save();
+				}
+				//add it to the todo
+				this.get("value").add("tag", tag).save();
+				//reset tagLabel
+				this.set("tagLabel", "");
+			}
 		}
+
 
 	});
 	
-	return declare([DomComponent, _WithDom], {
+	return declare([DomComponent, _WithDom, _WithDijit], {
 
 		constructor: function(params){
 			this.disabled = false;
@@ -45,6 +99,10 @@ define([
 			this._addComponents({
 				todoEditor: new TodoEditor(),
 				tagList: domConstruct.create("div"),
+				tagPicker: new ComboBox({
+					searchAttr: "label",
+					store: new Memory(), //since a store is mandatory at creation
+				}),
 			});
 		},
 		_render: function(){
@@ -52,6 +110,7 @@ define([
 			//place components
 			this._placeComponent(this._components.todoEditor);
 			this._placeComponent(this._components.tagList);
+			this._placeComponent(this._components.tagPicker);
 		},
 		_bind: function() {
 			this.own(
@@ -60,6 +119,18 @@ define([
 				}),
 				new binding.Stateful2InnerHtml(this._presenter, this._components.tagList, {
 					sourceProp: "tagsString",
+				}),
+				new binding.ValueSync(this._presenter, this._components.tagPicker, {
+					sourceProp: "tagLabel",
+					targetProp: "value",
+				}),
+				new binding.Value(this._presenter, this._components.tagPicker, {
+					sourceProp: "remainingTagsStore",
+					targetProp: "store",
+				}),
+				new binding.Event(this._components.tagPicker, this._presenter, {
+					event: "blur",
+					method: "tagLabelSubmited",
 				})
 			);
 		},
